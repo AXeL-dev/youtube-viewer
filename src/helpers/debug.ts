f5a67bbe1f4a58baf8a7cc9f2b6411c12ff412bc
AXeL-dev <contact.axel.dev@gmail.com>
2020-02-23 17:03:37 +0100

    Update debug.ts
    Code refactoring

--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,12 +1,7 @@
 import process from "process";
 
-const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
-export default function isDev(): boolean
-{
-  return development;
-}
+export const isDevEnv: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 export function debug(message: string, ...params: any) {
-  isDev() && console.log(message, ...params);
+  isDevEnv && console.log(message, ...params);
 }
