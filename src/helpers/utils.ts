b6ad1a8d65ef5d22758c899d11bcc9586cd91cf6
AXeL-dev <contact.axel.dev@gmail.com>
2020-06-19 18:24:47 +0100

    Update utils.ts
    Remove useless semicolons

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -194,14 +194,14 @@ export function memorySizeOf(obj: any) {
           }
       }
       return bytes;
-  };
+  }
 
   function formatByteSize(bytes: number) {
       if(bytes < 1024) return bytes + " bytes";
       else if(bytes < 1048576) return(bytes / 1024).toFixed(3) + " KiB";
       else if(bytes < 1073741824) return(bytes / 1048576).toFixed(3) + " MiB";
       else return(bytes / 1073741824).toFixed(3) + " GiB";
-  };
+  }
 
   return formatByteSize(sizeOf(obj));
 }
