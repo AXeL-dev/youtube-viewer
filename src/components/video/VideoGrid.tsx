55346aff0367d7894d6924cfff07c69e0e9c9d51
AXeL-dev <contact.axel.dev@gmail.com>
2020-06-26 20:58:04 +0100

    Update VideoGrid.tsx
    Fix long press click issue on pull to refresh

--- src/components/video/VideoGrid.tsx
+++ src/components/video/VideoGrid.tsx
@@ -4,6 +4,7 @@ import Box from '@material-ui/core/Box';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { Video } from '../../models/Video';
 import Media from './Media';
+import { debug } from '../../helpers/debug';
 
 interface VideoGridProps {
   loading?: boolean;
@@ -15,22 +16,37 @@ interface VideoGridProps {
 
 export default function VideoGrid(props: VideoGridProps) {
   const { videos, loading = false, maxPerChannel = 9, maxSkeletons = 9, onVideoClick } = props;
+  const [preventLongPress, setPreventLongPress] = React.useState(false);
   const style = {
     grid: {
       minWidth: '428px'
     }
   };
 
-  const cancelEvent = (event: any) => {
+  const handleMouseEvent = (event: any) => {
+    debug(event.type, { preventLongPress: preventLongPress });
+    if (event.type === 'mousedown') {
+      setPreventLongPress(false); // always reset preventLongPress state on mousedown
+      setTimeout(() => {
+        // delay of 200 ms used here to wait for the click event
+        // if it fires immediately after the mousedown event then preventLongPress value will be false, otherwise it's probably a long press click
+        setPreventLongPress(true);
+      }, 200);
+    } else {
+      if (!preventLongPress) {
+        return true;
+      }
+      setPreventLongPress(false);
+    }
     event.stopPropagation();
     event.preventDefault();
     return false;
   };
 
   return (
-    <Grid container style={style.grid}>
+    <Grid container style={style.grid} onMouseDown={(event: any) => handleMouseEvent(event)} onClickCapture={(event: any) => handleMouseEvent(event)}>
       {(loading ? Array.from(new Array(Math.min(maxPerChannel, maxSkeletons))) : videos.slice(0, maxPerChannel)).map((item, index) => (
-        <Box key={index} width={210} marginRight={0.5} marginBottom={3} draggable="false" onMouseDown={(event: any) => cancelEvent(event)}>
+        <Box key={index} width={210} marginRight={0.5} marginBottom={3} draggable="false">
           {item ? (
             <Media item={item} onClick={onVideoClick}></Media>
           ) : (
