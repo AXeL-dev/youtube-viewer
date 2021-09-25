6a3230cbb6553ccd47fb4b24799c6a8e95952791
AXeL-dev <contact.axel.dev@gmail.com>
2021-09-25 13:52:07 +0100

    chore: add time to webextension log messages

--- src/ui/components/webext/Background/logger.ts
+++ src/ui/components/webext/Background/logger.ts
@@ -1,7 +1,12 @@
 const { REACT_APP_DEBUG } = process.env;
 
+function time() {
+  const now = new Date();
+  return `[${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}]`;
+}
+
 export function log(message: any, ...params: any) {
   if (REACT_APP_DEBUG) {
-    console.log(message, ...params);
+    console.log(time(), message, ...params);
   }
 }
