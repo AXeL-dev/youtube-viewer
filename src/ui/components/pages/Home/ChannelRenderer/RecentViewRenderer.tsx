5686551accd9959832fcfbd095a306ea9d2c0401
AXeL-dev <contact.axel.dev@gmail.com>
2021-09-26 16:52:59 +0100

    refactor: optimize videos filtering on recent view renderer

--- src/ui/components/pages/Home/ChannelRenderer/RecentViewRenderer.tsx
+++ src/ui/components/pages/Home/ChannelRenderer/RecentViewRenderer.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useAppSelector } from 'store';
 import { selectSettings } from 'store/selectors/settings';
 import { getDateBefore } from 'helpers/utils';
 import DefaultRenderer, { DefaultRendererProps } from './DefaultRenderer';
-import {
-  selectViewedVideos,
-  selectWatchLaterVideos,
-} from 'store/selectors/videos';
+import { selectVideos } from 'store/selectors/videos';
 
 export interface RecentViewRendererProps
   extends Omit<DefaultRendererProps, 'publishedAfter'> {}
@@ -14,16 +11,22 @@ export interface RecentViewRendererProps
 function RecentViewRenderer(props: RecentViewRendererProps) {
   const { channel } = props;
   const settings = useAppSelector(selectSettings);
-  const viewedVideos = useAppSelector(selectViewedVideos(channel));
-  const watchLaterVideos = useAppSelector(selectWatchLaterVideos(channel));
-  const excludedVideosIds = [
-    ...(settings.recentVideosDisplayOptions.hideViewedVideos
-      ? viewedVideos.map(({ id }) => id)
-      : []),
-    ...(settings.recentVideosDisplayOptions.hideWatchLaterVideos
-      ? watchLaterVideos.map(({ id }) => id)
-      : []),
-  ];
+  const videos = useAppSelector(selectVideos(channel));
+  const { hideViewedVideos, hideWatchLaterVideos } =
+    settings.recentVideosDisplayOptions;
+  const excludedVideosIds = useMemo(
+    () => [
+      ...(hideViewedVideos
+        ? videos.filter(({ isViewed }) => isViewed).map(({ id }) => id)
+        : []),
+      ...(hideWatchLaterVideos
+        ? videos
+            .filter(({ isToWatchLater }) => isToWatchLater)
+            .map(({ id }) => id)
+        : []),
+    ],
+    [videos, hideViewedVideos, hideWatchLaterVideos]
+  );
   const publishedAfter = getDateBefore(
     settings.recentVideosSeniority
   ).toISOString();
