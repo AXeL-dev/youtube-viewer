8c3c60ffe8512b83435bd6cbf71d16e14bf1e162
AXeL-dev <contact.axel.dev@gmail.com>
2022-03-17 21:02:45 +0100

    chore: require an API key on first use

--- src/ui/components/pages/Settings/Alerts.tsx
+++ src/ui/components/pages/Settings/Alerts.tsx
@@ -6,7 +6,7 @@ import { setSettings } from 'store/reducers/settings';
 import { selectApp } from 'store/selectors/app';
 import { Settings } from 'types';
 
-const { REACT_APP_YOUTUBE_API_KEY: temporaryApiKey } = process.env;
+const { REACT_APP_YOUTUBE_API_KEY: defaultApiKey } = process.env;
 
 interface AlertsProps {
   settings: Settings;
@@ -17,9 +17,9 @@ export default function Alerts(props: AlertsProps) {
   const app = useAppSelector(selectApp);
   const dispatch = useAppDispatch();
 
-  const handleUseTemporaryKey = (event: MouseEvent) => {
+  const handleResetApiKey = (event: MouseEvent) => {
     event.preventDefault();
-    dispatch(setSettings({ apiKey: temporaryApiKey }));
+    dispatch(setSettings({ apiKey: defaultApiKey }));
   };
 
   return (
@@ -30,25 +30,20 @@ export default function Alerts(props: AlertsProps) {
         closable
         syncOpen
       >
-        In order to use this app you need a Youtube API key!{' '}
-        <Link
-          href="#"
-          color="secondary"
-          rel="noopener"
-          onClick={handleUseTemporaryKey}
-        >
-          Use a temporary key.
-        </Link>
-      </Alert>
-      <Alert
-        open={app.loaded && settings.apiKey === temporaryApiKey}
-        severity="warning"
-        closable
-        syncOpen
-      >
-        You are using a temporary API key, which has a limited quota & might be
-        used by other users. It is recommended to create your own key for daily
-        use.
+        In order to use this app you need a Youtube API key!
+        {defaultApiKey ? (
+          <>
+            {' '}
+            <Link
+              href="#"
+              color="secondary"
+              rel="noopener"
+              onClick={handleResetApiKey}
+            >
+              Reset API key.
+            </Link>
+          </>
+        ) : null}
       </Alert>
     </>
   );
