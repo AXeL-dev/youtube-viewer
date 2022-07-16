3bc6bfd8f4df626eb4402139403d839572336ddc
AXeL-dev <contact.axel.dev@gmail.com>
2022-07-16 14:54:20 +0000

    Deploying to gh-pages from  @ af75e1e16453ba9d6991e31c1f0aff9a2161dd48 🚀

--- content.js
+++ content.js
@@ -2,13 +2,40 @@
 
 (function () {
   function getMeta(name) {
-    return document.querySelector('meta[itemprop="' + name + '"]').content;
+    const query = document.querySelector('meta[itemprop="' + name + '"]');
+    return query ? query.content : undefined;
+  }
+
+  function getVideoId(url) {
+    let videoId = null;
+    if (url.indexOf('v=') !== -1) {
+      /**
+       * Example:
+       *    https://www.youtube.com/watch?v=aZ-dSpfdHok
+       *    https://www.youtube.com/watch?v=aZ-dSpfdHok&feature=feedrec_grec_index
+       */
+      videoId = url.split('v=')[1];
+      const index = videoId.indexOf('&');
+      if (index !== -1) {
+        videoId = videoId.substring(0, index);
+      }
+    } else if (url.indexOf('/') !== -1) {
+      /**
+       * Example:
+       *    https://youtu.be/aZ-dSpfdHok
+       *    https://youtu.be/PqkaBUmJpq8?list=PLmmPGQQTKzSZSPd3pa6q9UQ-PkeCx1fam
+       */
+      const link = url.split('?')[0];
+      const params = link.split('/');
+      videoId = params[params.length - 1];
+    }
+    return videoId;
   }
 
   function handleMessage(request, sender) {
     switch (request.message) {
       case 'getVideoInfos': {
-        const videoId = getMeta('videoId');
+        const videoId = getMeta('videoId') || getVideoId(window.location.href);
         const channelId = getMeta('channelId');
         const datePublished = getMeta('datePublished');
         port.postMessage({
@@ -34,6 +61,6 @@
     }
   })();
 
-  const port = browserAPI.runtime.connect({ name: 'cs-port' });
+  const port = browserAPI.runtime.connect({ name: 'youtube-viewer-cs' });
   port.onMessage.addListener(handleMessage);
 })();
