61aa6c0e5684a9d3d3042b219566049001bed29f
axel-dev <contact.axel.dev@gmail.com>
2022-11-21 18:15:16 +0100

    chore(home): tweak videos skeleton

--- src/ui/components/pages/Home/ChannelRenderer/ChannelVideos/VideoSkeleton.tsx
+++ src/ui/components/pages/Home/ChannelRenderer/ChannelVideos/VideoSkeleton.tsx
@@ -10,12 +10,16 @@ export default function VideoSkeleton(props: VideoSkeletonProps) {
   const { width = '100%', height = 120 } = props;
 
   return (
-    <>
+    <Box height={220}>
       <Skeleton variant="rectangular" width={width} height={height} />
-      <Box pt={0.5}>
-        <Skeleton />
-        <Skeleton width="60%" />
+      <Box pt={0.75}>
+        <Skeleton height={20} />
+        <Skeleton height={20} width="60%" />
       </Box>
-    </>
+      <Box pt={1}>
+        <Skeleton height={14} width="40%" style={{ marginBottom: 6 }} />
+        <Skeleton height={14} width="60%" />
+      </Box>
+    </Box>
   );
 }
